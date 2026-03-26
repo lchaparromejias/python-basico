cfb90fd4b0f7cf3537516da259c046cdaf9e32dd
lchaparromejias <luischaparro.dev@gmail.com>
2026-03-26 13:10:43 +0100

    He hecho un cambio en la calculadora, he añadido otro proyecto

--- python-basic-calculator/BasicCalculator.py
+++ python-basic-calculator/BasicCalculator.py
@@ -1,32 +1,34 @@
 # ==========================================
 # Autor: Luis Chaparro Mejías
-# Fecha: 25 de marzo de 2026
+# Fecha: 25/26 de marzo de 2026 
 # Proyecto: Calculadora Básica de Python
 # ==========================================
 
-# Mensaje de bienvenida
 print("Bienvenid@ a mi calculadora básica de Python")
 
-# Captura de datos del usuario
+# Captura de datos
 numero1 = int(input("Introduce el primer número: "))
 numero2 = int(input("Introduce el segundo número: "))
-
-# Captura y normalización de la operación (evita errores por mayúsculas)
 operacion = str(input("Elige la operación (Suma, Resta, Multiplicación, División): ")).capitalize()
 
-# Lógica principal de la calculadora
 numero = 0
 
+# Lógica con control de errores
 if operacion == "Suma":
     numero = numero1 + numero2
 elif operacion == "Resta":
     numero = numero1 - numero2
 elif operacion == "División" or operacion == "Division":
-    numero = numero1 / numero2
+    if numero2 == 0:
+        print("Error: No se puede dividir entre cero.")
+        exit()
+    else:
+        numero = numero1 / numero2
 elif operacion == "Multiplicación" or operacion == "Multiplicacion":
     numero = numero1 * numero2
 else:
-    print("Por favor, escribe la operación adecuada.")
+    print("Error: Por favor, escribe la operación adecuada.")
+    exit() 
 
-# Salida del resultado final
+# Salida del resultado final (solo si no hubo errores antes)
 print(f"La solución de hacer {numero1} {operacion} {numero2} es {numero}")
---
+++ python-rock-paper-scissors/rps_game.py
@@ -0,0 +1,11 @@
+# ==========================================
+# Autor: Luis Chaparro Mejías
+# Proyecto: Juego Piedra, Papel o Tijera
+# Fecha: 26 de marzo de 2026
+# ==========================================
+import random
+
+# 1. Mensaje de bienvenida
+print("Bienvenid@ al juego de Piedra, Papel o Tijera")
+
+# 2. Definimos las opciones en una lista
